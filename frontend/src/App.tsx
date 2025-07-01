7bd41620f56d2230035d43fcd01242728fb24c9e
Reesey275 <reesey275@thenagrygamershow.com>
2025-07-01 03:16:17 -0400

    test: add a11y checks to Playwright

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import Login from './components/Login';
 
 export default function App() {
-  return (
-    <div>
-      <h1>DevOnboarder</h1>
-      <Login />
-    </div>
-  );
+    return (
+        <main>
+            <h1>DevOnboarder</h1>
+            <Login />
+        </main>
+    );
 }
