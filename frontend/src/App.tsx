dca1d8c28c2ec36e541d1a59f434273454148a59
RabbitAlbatross <143070183+RabbitAlbatross@users.noreply.github.com>
2025-09-28 21:34:54 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,22 +2,27 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import PublicLandingPage from "./components/PublicLandingPage";
 import ProtectedDashboard from "./components/ProtectedDashboard";
+import { ThemeProvider } from "./hooks/useTheme";
+
+// Import theme styles
 import "./styles/themes.css";
 
 export default function App() {
     return (
-        <Router>
-            <div className="min-h-screen">
-                <Header />
-                <Routes>
-                    <Route path="/" element={<PublicLandingPage />} />
-                    <Route path="/dashboard" element={<ProtectedDashboard />} />
-                    <Route path="/login/discord/callback" element={<ProtectedDashboard />} />
-                    {/* Redirect legacy routes */}
-                    <Route path="/admin" element={<ProtectedDashboard />} />
-                    <Route path="/staff" element={<ProtectedDashboard />} />
-                </Routes>
-            </div>
-        </Router>
+        <ThemeProvider>
+            <Router>
+                <div className="min-h-screen theme-bg">
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<PublicLandingPage />} />
+                        <Route path="/dashboard" element={<ProtectedDashboard />} />
+                        <Route path="/login/discord/callback" element={<ProtectedDashboard />} />
+                        {/* Redirect legacy routes */}
+                        <Route path="/admin" element={<ProtectedDashboard />} />
+                        <Route path="/staff" element={<ProtectedDashboard />} />
+                    </Routes>
+                </div>
+            </Router>
+        </ThemeProvider>
     );
 }
